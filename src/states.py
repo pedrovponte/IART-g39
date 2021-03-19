e1fe669ba639ae0579c839e24ebd31db676950ee
Pedro Ferreira <up201806506@fe.up.pt>
2021-03-19 11:09:35 +0000

    Some changes in states file

--- src/states.py
+++ src/states.py
@@ -1,19 +1,35 @@
+class Node:
+    def __init__(self, state, parent, operator, depth, cost):
+        # Contains the state of the node
+        self.state = state
+        # Contains the node that generated this node
+        self.parent = parent
+        # Contains the operation that generated this node from the parent
+        self.operator = operator
+        # Contains the depth of this node (parent.depth +1)
+        self.depth = depth
+        # Contains the path cost of this node from depth 0. Not used for depth/breadth first.
+        self.cost = cost
+
+        self.heuristic=None
+
 # returns true if final state is reached
 def objectiveTest(board):
     for row in board:
         for cel in row:
             if(cel[0]=='F'):
                 return False
-            
-            
+            if len(cel)>2:
+                if cel[1]!=cel[3]:
+                    return False
     return True
-
+"""
 print(objectiveTest([
-		['X','-','-','I'],
-		['I','-','-','-'],
-		['X','F','-','-'],
-		['X','F','-','-']
-		]))
+		['X','-','-','-'],
+		['-','-','-','-'],
+		['X','IOFO','-','-'],
+		['X','ICFC','-','-']
+		]))"""
 
 # conditions so it can move to given direction (must have empty or final state in the op direction of at least one cell)
 def precond(board, op):
@@ -37,15 +53,17 @@ def precond(board, op):
                     if col!=len(board)-1 and board[row][col+1]!="X" and board[row][col+1][0]!="I":
                         return True
     return False
-
+"""
 print(precond([
 		['X','-','-','-'],
 		['-','I','-','I'],
 		['X','F','-','-'],
 		['X','F','-','-']
-		], "right"))
+		], "right"))"""
 
 def effects(board,op):
+    if precond(board,op)==False:
+        return None
     if(op=="up"):
         for row in range(len(board)-1,0,-1):
             for col in range(len(board)):
@@ -65,7 +83,7 @@ def effects(board,op):
                         else:
                             board[row-1][col]=board[row][col]+board[row-1][col]
                             board[row][col]="-"
-    if(op=="down"):
+    elif(op=="down"):
         for row in range(len(board)-1):
             for col in range(len(board)):
                 if board[row][col][0]=="I":
@@ -84,7 +102,7 @@ def effects(board,op):
                         else:
                             board[row+1][col]=board[row][col]+board[row+1][col]
                             board[row][col]="-"
-    if(op=="right"):
+    elif(op=="right"):
         for col in range(len(board)-1):
             for row in range(len(board)):
                 if board[row][col][0]=="I":
@@ -103,7 +121,7 @@ def effects(board,op):
                         else:
                             board[row][col+1]=board[row][col]+board[row][col+1]
                             board[row][col]="-"
-    if(op=="left"):
+    elif(op=="left"):
         for col in range(len(board)-1,0,-1):
             for row in range(len(board)):
                 if board[row][col][0]=="I":
@@ -124,9 +142,67 @@ def effects(board,op):
                             board[row][col]="-"
     return board
 
-print(effects([
+"""print(effects([
 		['X','F1','-','I4'],
 		['-','I1','F2','-'],
 		['X','F2','F3','I2'],
 		['X','-','I3','-']
-		], "left"))
+		], "left"))"""
+
+def expand_node(node):
+    """Returns a list of expanded nodes"""
+    expanded_nodes = []
+    parentState = node.state
+    print("UP")
+    print(effects(parentState,"up"))
+    print("D")
+    print(effects(parentState,"down"))
+    print("L")
+    print(effects(parentState,"left"))
+    print("R")
+    print(effects(parentState,"right"))
+    expanded_nodes.append(Node(effects(node.state,"up"), node, "up", node.depth + 1, 0))
+    expanded_nodes.append(Node(effects(node.state,"down"), node, "down", node.depth + 1, 0))
+    expanded_nodes.append(Node(effects(node.state,"left"), node, "left", node.depth + 1, 0))
+    expanded_nodes.append(Node(effects(node.state,"right"), node, "right", node.depth + 1, 0))
+    # Filter the list and remove the nodes that are impossible (move function returned None)
+    expanded_nodes = [node for node in expanded_nodes if node.state != None]  # list comprehension!
+    n=0
+    #for node in expanded_nodes:
+        #print(n)
+        #print(node.state)
+        #n+=1
+    return expanded_nodes
+
+def bfs(start):
+    """Performs a breadth first search from the start state to the goal"""
+    # A list (can act as a queue) for the nodes.
+    start_node=Node(start, None, None, 0, 0)
+    fringe=[]
+    fringe.append(start_node)
+    current=fringe.pop(0)
+    path=[]
+    
+    for i in range(3):
+        print(current.state)
+        fringe.extend(expand_node(current))
+        current=fringe.pop(0)
+    while(current.parent!=None):
+        print("H")
+        path.insert(0,current.operator)
+        current=current.parent
+    return path
+
+print(expand_node(Node([['X','-','-','IY'],
+		['IB','-','-','-'],
+		['X','FB','-','-'],
+		['X','FY','-','-']
+		], None, None, 0, 0))
+		)
+
+"""bfs([
+		['X','-','-','IY'],
+		['IB','-','-','-'],
+		['X','FB','-','-'],
+		['X','FY','-','-']
+		])"""
