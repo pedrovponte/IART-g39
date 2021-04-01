dfec69fa1f8867a93b182fe891965b246efd6d88
Pedro Ferreira <up201806506@fe.up.pt>
2021-04-01 20:05:37 +0000

    Commented States File

--- src/states.py
+++ src/states.py
@@ -22,15 +22,16 @@ class Node:
 def objectiveTest(board):
     for row in board:
         for cel in row:
+            # If there is any cell starting with "F" it means there is one final cell empty
             if(cel[0]=='F'):
                 return False
+            # If the lenght of the cell is higher than 2 it means a cell is on top of a final cell
             if len(cel)>2:
                 if cel[1]!=cel[3]:
                     return False
     return True
 
-
-# =============================================================================
+# ===========================Test purposes=====================================
 # print(objectiveTest([
 # 		['X','-','-','-'],
 # 		['-','-','-','-'],
@@ -42,7 +43,10 @@ def objectiveTest(board):
 
 # conditions so it can move to given direction (must have empty or final state in the op direction of at least one cell)
 def precond(oldboard, op):
+    # Copies the passed argument so it can be modified and studied
     board = [x[:] for x in oldboard]
+
+    # Preconditions for movement: Have at least one piece that can move in the asked direction (doesn't have a wall or end of board in asked direction)
     for row in range(len(board)):
         for col in range(len(board)):
             # means it is movable piece
@@ -50,22 +54,19 @@ def precond(oldboard, op):
                 if op=="up":
                     if row!=0 and board[row-1][col]!="X" and board[row-1][col][0]!="I":
                         return True
-            if board[row][col][0]=="I":
                 if op=="down":
                     if row!=len(board)-1 and board[row+1][col]!="X" and board[row+1][col][0]!="I":
                         return True
-            if board[row][col][0]=="I":
                 if op=="left":
                     if col!=0 and board[row][col-1]!="X" and board[row][col-1][0]!="I":
                         return True
-            if board[row][col][0]=="I":
                 if op=="right":
                     if col!=len(board)-1 and board[row][col+1]!="X" and board[row][col+1][0]!="I":
                         return True
     return False
 
 
-# =============================================================================
+# ===========================Test purposes=====================================
 # print(precond([
 # 		['X','-','-','-'],
 # 		['-','I','-','I'],
@@ -74,30 +75,45 @@ def precond(oldboard, op):
 # 		], "right"))
 # =============================================================================
 
-    
+# Movement effects if preconditions are satisfied
 def effects(oldboard,op):
+
+    # Copies the passed argument so it can be modified and studied
     board = [x[:] for x in oldboard]
+
+    # Verifies if the preconditions are satisfied
     if precond(board,op)==False:
         return None
+    
+    # Auxiliar board
     newboard=[]
     if(op=="up"):
+        # Since each movement makes the pieces go until there is no more room to move, then if the previous board is the same as the current it means no more movements can be made in this direction
         while(newboard!=board):
             newboard = [x[:] for x in board]
             for row in range(len(board)-1,0,-1):
                 for col in range(len(board)):
                     if board[row][col][0]=="I":
-                        # Top is not F
+                        # Top piece is not final
                         if board[row-1][col]=="-":
+                            # Current piece is on top of another final piece
                             if len(board[row][col])>2:
                                 board[row-1][col]=board[row][col][:2]
                                 board[row][col]=board[row][col][2:]
+
+                            # Current piece is not on top of another final piece
                             else:
                                 board[row-1][col]=board[row][col]
                                 board[row][col]="-"
+
+                        # Top piece is final
                         if board[row-1][col][0]=="F":
+                            # Current piece is on top of another final piece
                             if len(board[row][col])>2:
                                 board[row-1][col]=board[row][col][:2]+board[row-1][col]
                                 board[row][col]=board[row][col][2:]
+
+                            # Current piece is not on top of another final piece
                             else:
                                 board[row-1][col]=board[row][col]+board[row-1][col]
                                 board[row][col]="-"             
@@ -108,18 +124,26 @@ def effects(oldboard,op):
             for row in range(len(board)-1):
                 for col in range(len(board)):
                     if board[row][col][0]=="I":
-                        # Top is not F
+                        # Bottom is not final
                         if board[row+1][col]=="-":
+                            # Current piece is on top of another final piece
                             if len(board[row][col])>2:
                                 board[row+1][col]=board[row][col][:2]
                                 board[row][col]=board[row][col][2:]
+
+                            # Current piece is not on top of another final piece
                             else:
                                 board[row+1][col]=board[row][col]
                                 board[row][col]="-"
+
+                        # Bottom is final
                         if board[row+1][col][0]=="F":
+                            # Current piece is on top of another final piece
                             if len(board[row][col])>2:
                                 board[row+1][col]=board[row][col][:2]+board[row+1][col]
                                 board[row][col]=board[row][col][2:]
+
+                            # Current piece is not on top of another final piece
                             else:
                                 board[row+1][col]=board[row][col]+board[row+1][col]
                                 board[row][col]="-"
@@ -130,18 +154,25 @@ def effects(oldboard,op):
             for col in range(len(board)-1):
                 for row in range(len(board)):
                     if board[row][col][0]=="I":
-                        # Top is not F
+                        # Current piece is on top of another final piece
                         if board[row][col+1]=="-":
+                            # Current piece is on top of another final piece
                             if len(board[row][col])>2:
                                 board[row][col+1]=board[row][col][:2]
                                 board[row][col]=board[row][col][2:]
+
+                            # Current piece is not on top of another final piece
                             else:
                                 board[row][col+1]=board[row][col]
                                 board[row][col]="-"
+                        # Current piece is on top of another final piece
                         if board[row][col+1][0]=="F":
+                            # Current piece is on top of another final piece
                             if len(board[row][col])>2:
                                 board[row][col+1]=board[row][col][:2]+board[row][col+1]
                                 board[row][col]=board[row][col][2:]
+
+                            # Current piece is not on top of another final piece
                             else:
                                 board[row][col+1]=board[row][col]+board[row][col+1]
                                 board[row][col]="-"
@@ -152,25 +183,32 @@ def effects(oldboard,op):
             for col in range(len(board)-1,0,-1):
                 for row in range(len(board)):
                     if board[row][col][0]=="I":
-                        # Top is not F
+                        # Current piece is on top of another final piece
                         if board[row][col-1]=="-":
+                            # Current piece is on top of another final piece
                             if len(board[row][col])>2:
                                 board[row][col-1]=board[row][col][:2]
                                 board[row][col]=board[row][col][2:]
+
+                            # Current piece is not on top of another final piece
                             else:
                                 board[row][col-1]=board[row][col]
                                 board[row][col]="-"
+                        # Current piece is on top of another final piece
                         if board[row][col-1][0]=="F":
+                            # Current piece is on top of another final piece
                             if len(board[row][col])>2:
                                 board[row][col-1]=board[row][col][:2]+board[row][col-1]
                                 board[row][col]=board[row][col][2:]
+
+                            # Current piece is not on top of another final piece
                             else:
                                 board[row][col-1]=board[row][col]+board[row][col-1]
                                 board[row][col]="-"
         return board
 
 
-# =============================================================================
+# ===========================Test purposes=====================================
 # node1 = [['X','-','-','-'],
 # 		['IB','-','I2','IY'],
 # 		['X','FB','-','IJ'],
@@ -179,24 +217,22 @@ def effects(oldboard,op):
 # print(effects(node1,"left"))
 # =============================================================================
 
-
+# Expands a node and gets all possible nodes it can reach 
 def expand_node(node):
-    """Returns a list of expanded nodes"""
+
+    # List of expanded nodes to be returned
     expanded_nodes = []
+
+    # Gets expanded nodes with movement up, donw, left and right
     expanded_nodes.append(Node(effects(node.state,"up"), node, "up", node.depth + 1, 1))
     expanded_nodes.append(Node(effects(node.state,"down"), node, "down", node.depth + 1, 1))
     expanded_nodes.append(Node(effects(node.state,"left"), node, "left", node.depth + 1, 1))
     expanded_nodes.append(Node(effects(node.state,"right"), node, "right", node.depth + 1, 1))
+
     # Filter the list and remove the nodes that are impossible (move function returned None)
-    expanded_nodes = [node for node in expanded_nodes if node.state != None]  # list comprehension!
+    expanded_nodes = [node for node in expanded_nodes if node.state != None]
+    
     return expanded_nodes
 
-
-def h(state):
-    dmatch=0
-    for i in range(0,9):
-        if (objectiveTest(state.state)!=True):
-            dmatch+=1
-    state.heuristic=dmatch
 
 
